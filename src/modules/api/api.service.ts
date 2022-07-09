49758e2040d8d1ea9fa8f01d5eab7ed5f07155d4
Tal Rofe <tal@exlint.io>
2022-07-09 08:22:18 +0300

    refactor: 💡 revert api service
    revert api service

--- src/modules/api/api.service.ts
+++ src/modules/api/api.service.ts
@@ -23,85 +23,16 @@ export class ApiService {
 	}
 
 	public async getGroupData(groupId: string) {
-		/* 		const groupDataResponse = await axios.get<IGetGroupPoliciesResponse>(
+		const groupDataResponse = await axios.get<IGetGroupPoliciesResponse>(
 			`/user/groups/get-group/${groupId}`,
-		); */
-
-		await Promise.resolve(groupId);
-
-		const groupDataResponse = {
-			data: {
-				id: 'yazif',
-				policies: [
-					{
-						id: 'yazif',
-						library: 'eslint',
-						label: 'joseha',
-						configuration: {
-							root: true,
-							env: {
-								node: true,
-							},
-							parserOptions: {
-								ecmaVersion: 12,
-								sourceType: 'module',
-							},
-							rules: {
-								'max-lines': ['error', { max: 2, skipBlankLines: true, skipComments: true }],
-								'capitalized-comments': ['error', 'always'],
-							},
-							__EXLINT_FILES_PATTERN__: 'test.js',
-						},
-					},
-					{
-						id: 'yazif',
-						library: 'inflint',
-						label: 'joseha',
-						configuration: {
-							rules: {
-								'**/*': [2, 'kebab-case'],
-							},
-						},
-					},
-					{
-						id: 'yazif',
-						library: 'stylelint',
-						label: 'joseha',
-						configuration: {
-							rules: {
-								'color-named': ['never'],
-							},
-							__EXLINT_FILES_PATTERN__: 'test.css',
-						},
-					},
-					{
-						id: 'yazif',
-						library: 'prettier',
-						label: 'joseha',
-						configuration: {
-							tabWidth: 4,
-							printWidth: 110,
-							useTabs: true,
-							semi: true,
-							singleQuote: true,
-							quoteProps: 'consistent',
-							trailingComma: 'all',
-							bracketSpacing: true,
-							arrowParens: 'always',
-							bracketSameLine: false,
-						},
-					},
-				],
-			},
-		};
+		);
 
 		return groupDataResponse.data as IGetGroupPoliciesResponse;
 	}
 
 	public async hasValidToken() {
 		try {
-			//axios.get('/user/auth/verify-token');
-			await Promise.resolve();
+			await axios.get('/user/auth/verify-token');
 
 			return true;
 		} catch {
